81d5184e20f7a0b6026209948c904e1d5158e208
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-09-03 21:30:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
+
+const user = require("./models/User")
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+app.use(express.json())
+app.use(require("./routes/auth"));
+
+
+
+
+const meddleware = (req, res, next) => {
+  console.log("meddleware called");
+  next();
+};
+
+app.get("/", meddleware, (req, res) => {
+  res.send("health okay");
+  console.log("health okay okay");
+});
+
+app.listen(process.env.PORT, (error) => {
+  if (error) throw error;
+});
