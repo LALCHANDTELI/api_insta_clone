87b9592fb3b1b4d5fea2b486726244368dfe3fc4
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-09-03 21:32:57 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+name:{
+  type:String,
+  required:true
+},
+  email: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  username:{
+    type: String,
+    required: true,
+    unique:true
+  },
+
+  photo:{
+    type: String,
+    default:"no photo"
+  },
+  pin: {
+    type: String,
+    required: true
+  }
+})
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("pin")) {
+    this.pin = await bcrypt.hash(this.pin, 12);
+  }
+  next();
+});
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
