2d614ac3f4298816d7605791468d30411daaf3b6
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-09-04 16:44:36 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
+const cloudinary = require("cloudinary").v2;
 
 mongoose.connect(process.env.DATABASE).catch((error) => {
   console.log(error);
+});
+
+
+cloudinary.config({
+  cloud_name: "dcxhqv5lu",
+  api_key: "431785148661475",
+  api_secret: "EZE1Go9aONy0pkA4M5YlNSBTUVY",
 });
