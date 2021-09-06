2cce1343c7cfccd1b82b55267cdbbee17d8cc2d5
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-09-06 23:54:52 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -79,4 +79,25 @@ router.post("/checknewuser", async (req, res) => {
 });
 
 
+
+router.post("/login", async (req, res) => {
+    try {
+    const  {email,pin} = await req.body;
+      
+    if (!email || !pin) {
+      return res.send("please fill the email");
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.send(user);
+    }else{
+        return res.send("wrong email and pin");
+    }
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+
 module.exports = router;
