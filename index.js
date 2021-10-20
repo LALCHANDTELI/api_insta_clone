bb3851db831fdcf611574a34bf3b03c3a7a255ae
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-10-20 11:09:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config({ path: "./config.env" });
 require("./db/conn");
 
 const user = require("./models/User")
-const sgMail = require('@sendgrid/mail')
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,25 +24,13 @@ const meddleware = (req, res, next) => {
 };
 
 app.get("/", meddleware, (req, res) => {
-   sgMail.setApiKey("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")
-  const msg = {
-    to: 'lalchandteli13@gmail.com', 
-    from: 'lalchandteli13@gmail.com', 
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
-  sgMail.send(msg)
-    .then(() => {
-       res.send("sent");
-    })
-    .catch((error) => {
-       res.send("error");
-    })
+  
+  
+ 
   
   
    console.log("health okay okay");
- 
+  res.send("health okay");
   
   
    
