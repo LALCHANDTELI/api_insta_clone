7b368d45d5d769b3fa8280801fca8d8d42843520
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-10-20 11:14:03 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@ router.get("/", (req, res) => {
    const sgMail = require('@sendgrid/mail')
    sgMail.setApiKey("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")
   const msg = {
-    to: 'lalchandteli13@gmail.com', 
+    to: 'pradeep05112001@gmail.com', 
     from: 'lalchandteli13@gmail.com', 
     subject: 'Sending with SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
