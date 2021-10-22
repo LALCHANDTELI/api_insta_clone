092b9b26623baf8e57db47180ff35e73203ef768
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-10-23 01:29:31 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -127,7 +127,7 @@ router.post("/send_mail",async(req,res)=>{
       
         var mailOptions = {
         
-    from:[{name:'insta clone',address:'assistanceforcecenter@gmail.com'}],
+    from:'assistanceforcecenter@gmail.com',
     to: 'lalchandteli13@gmail.com',
     subject: 'Sending Email using Node.js',
     text: 'That was easy!',
