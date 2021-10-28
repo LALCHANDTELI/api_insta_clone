5909e40c7c61435725bd466aa303964df3492287
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-10-28 14:45:09 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const PostSchema = new Schema({
 id:{
-   type: mongoose.ObjectId,
+   type: String,
     required: true,
     unique:true
   },
