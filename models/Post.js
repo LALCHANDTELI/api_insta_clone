20f0f48f8300d5dcb0741a2c696af978207b15c1
lal chand teli <42927241+LALCHANDTELI@users.noreply.github.com>
2021-10-28 23:30:01 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -3,20 +3,24 @@ const { Schema } = mongoose;
 const PostSchema = new Schema({
 user_id:{
    type: String,
-    required: true
+    required: true,
+   unique:false
   },
   username:{
     type: String,
-    required: true
+    required: true,
+     unique:false
   },
 
   photo:{
     type: String,
-     required: true
+     required: true,
+     unique:false
   },
   descriptions: {
     type: String,
-    required: true
+    required: true,
+     unique:false
   }
 })
 
